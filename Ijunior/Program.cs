ac649796c0c86a3b0d532a6d3a8c2b3b1fbd097a
HemyDev <97851451+Elezthem@users.noreply.github.com>
2023-08-26 13:26:19 +0300

    Add files via upload

---
+++ Ijunior/Program.cs
@@ -0,0 +1,27 @@
+﻿int[] numbers = new int[] { 1, 3, 5, 7, 11, 13, 17 };
+
+Console.WriteLine("Квадраты чисел");
+Console.WriteLine();
+Console.WriteLine("Дан ряд чисел:");
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.WriteLine(numbers[i]);
+}
+
+//
+
+Console.WriteLine();
+Console.WriteLine("Квадраты этих чисел:");
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] *= numbers[i];
+}
+
+foreach (int num in numbers)
+{
+    Console.WriteLine(num);
+}
+
+//
